50212059d6f0e1e45ec9c8c15959a5aafcab6b68
Anasks19 <anasks1999@gmail.com>
2023-10-15 16:35:05 +0000

    header file

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+/* prototypes functions to build _printf */
+int ft_strlen(char *str);
+int ft_putchar(va_list args);
+int ft_putstr(va_list args);
+int ft_putnbr(va_list args);
+int (*get_ft_spec(char c))(va_list args);
+int ft_print_binary(va_list args);
+int ft_putnbrs(int n);
+
+/* Struct to get the corresponding format specifier and prints it */
+/**
+ * format specifier - structure that gets the right format specifier "c"
+ * and points to the function that prints c
+ * @c: character that holds the format specifier
+ * @func: pointer to the function that prints depending on c.
+*/
+typedef struct format_specifier
+{
+	char c;
+	int (*func)(va_list args);
+} ft_spec;
+
+/* prototypes functions of _printf */
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
